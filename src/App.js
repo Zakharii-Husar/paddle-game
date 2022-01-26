688fc7f40c4e2a186daf8445654b01d05a889fd5
Zakharii-Husar <zakhariihusar@gmail.com>
2022-01-26 15:31:06 +0200

    cancel conditionals

--- src/App.js
+++ src/App.js
@@ -43,11 +43,7 @@ const App = () => {
     const [bricksLeft, setBricksLeft] = useState(rows * columns);
 
     const fullScreen = () => {
-        const element = containerRef.current;
-        if(element) element.requestFullscreen()
-        else if (element.mozRequestFullscreen) element.mozRequestFullscreen()
-        else if(element.webkitRequestFullscreen)element.webkitRequestFullscreen()
-        else if(element.msRequestFullscreen) element.msRequestFullscreen()
+        containerRef.current.requestFullscreen()
         window.screen.orientation.lock('landscape-primary');
     }
 
