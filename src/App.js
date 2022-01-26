5dbf199cd26c191be33555fe2e803148cfdb8655
Zakharii-Husar <zakhariihusar@gmail.com>
2022-01-26 14:50:44 +0200

    deprecated w prefixes

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ const App = () => {
 
     const fullScreen = () => {
         containerRef.current.requestFullscreen();
-        window.screen.lockOrientation('landscape-primary');
+        let locOrientation = window.screen.lockOrientation || window.screen.mozLockOrientation || window.screen.msLockOrientation || window.screen.orientation.lock;
+locOrientation('landscape');
         window.screen.orientation.lock('landscape-primary');
     }
 
