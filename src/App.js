79670adf57472ac50bec874247570aac34544e26
Zakharii-Husar <zakhariihusar@gmail.com>
2022-01-26 14:12:08 +0200

    start on touch start

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ const App = () => {
             className='menu'
             style={menuStyle} >
             <div className='level'>{notification}</div>
-            <button className='start' onClick={startGame} onTouchEnd={startGame}> {startBtnText} </button>
+            <button className='start' onClick={startGame} onTouchStart={startGame} onTouchEnd={startGame}> {startBtnText} </button>
         </div >
 
         <div className='panel' >
